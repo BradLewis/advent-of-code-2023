2ce4ee3ed6bc89182c3d88817b809337f652f2a7
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2023-12-05 19:21:19 -0500

    complete day 5 part 1

---
+++ day05/src/main.rs
@@ -0,0 +1,96 @@
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("failed to read input");
+    let result = part1(&input);
+    println!("part 1: {}", result);
+}
+
+#[derive(Debug, Copy, Clone)]
+struct Map {
+    source: usize,
+    destination: usize,
+    length: usize,
+}
+
+impl Map {
+    fn new(destination: usize, source: usize, length: usize) -> Self {
+        Self {
+            source,
+            destination,
+            length,
+        }
+    }
+
+    fn partner(&self, source: usize) -> Option<usize> {
+        if source >= self.source && source < self.source + self.length {
+            Some(self.destination + (source - self.source))
+        } else {
+            None
+        }
+    }
+}
+
+fn part1(input: &str) -> usize {
+    let (seed_section, sections) = input.split_once("\n\n").expect("failed to split input");
+    let seeds: Vec<_> = seed_section
+        .split(" ")
+        .filter_map(|s| s.parse::<usize>().ok())
+        .collect();
+
+    let map_collection: Vec<_> = sections
+        .split("\n\n")
+        .map(|s| {
+            s.lines()
+                .enumerate()
+                .filter_map(|(i, l)| {
+                    if i == 0 {
+                        return None;
+                    }
+                    let mut parts = l.split(" ");
+                    let source = parts.next()?.parse::<usize>().ok()?;
+                    let destination = parts.next()?.parse::<usize>().ok()?;
+                    let length = parts.next()?.parse::<usize>().ok()?;
+                    Some(Map::new(source, destination, length))
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect();
+
+    seeds
+        .iter()
+        .map(|s| {
+            let mut current = *s;
+            for map_group in map_collection.iter() {
+                let mut next = current;
+                for map in map_group.iter() {
+                    if let Some(partner) = map.partner(current) {
+                        next = partner;
+                        break;
+                    }
+                }
+                current = next;
+            }
+            current
+        })
+        .min()
+        .expect("should have min item")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let input = fs::read_to_string("test_input.txt").expect("failed to read test input");
+        let result = part1(&input);
+        assert_eq!(result, 35);
+    }
+
+    #[test]
+    fn test_map() {
+        let map = Map::new(52, 50, 48);
+        assert_eq!(map.partner(79), Some(81));
+    }
+}
