9db8782f8fc5de263c2ebe32df7f006e38cfc263
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2023-12-09 12:25:15 -0500

    complete day 9 part 1

---
+++ day09/src/main.rs
@@ -0,0 +1,72 @@
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("failed to read input file");
+    let result = part1(&input);
+    println!("part 1: {}", result);
+}
+
+fn part1(input: &str) -> i64 {
+    input.lines().map(parse_line).sum()
+}
+
+fn parse_line(line: &str) -> i64 {
+    let numbers: Vec<_> = line
+        .split_whitespace()
+        .map(|x| x.parse::<i64>().expect("failed to parse int"))
+        .collect();
+
+    next_value(&numbers)
+}
+
+fn next_value(numbers: &[i64]) -> i64 {
+    if numbers.iter().all(|&x| x == 0) {
+        return 0;
+    }
+    let next_row = next_row(numbers);
+    numbers.last().expect("no last entry in numbers") + next_value(&next_row)
+}
+
+fn next_row(numbers: &[i64]) -> Vec<i64> {
+    let mut next_row = vec![0; numbers.len() - 1];
+    for i in 0..numbers.len() - 1 {
+        next_row[i] = numbers[i + 1] - numbers[i];
+    }
+    next_row
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let input = fs::read_to_string("test_input.txt").expect("failed to read input file");
+        let result = part1(&input);
+        assert_eq!(result, 114);
+    }
+
+    #[test]
+    fn test_parse_line() {
+        let result = parse_line("0 3 6 9 12 15");
+        assert_eq!(result, 18);
+    }
+
+    #[test]
+    fn test_next_value() {
+        let result = next_value(&[1, 1, 1, 1]);
+        assert_eq!(result, 1);
+
+        let result = next_value(&[0, 3, 6, 9, 12, 15]);
+        assert_eq!(result, 18);
+    }
+
+    #[test]
+    fn test_next_row() {
+        let result = next_row(&[0, 3, 6, 9, 12, 15]);
+        assert_eq!(result, vec![3, 3, 3, 3, 3]);
+
+        let result = next_row(&[1, 1, 1, 1]);
+        assert_eq!(result, vec![0, 0, 0]);
+    }
+}
