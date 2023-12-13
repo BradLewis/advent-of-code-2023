588cc57fbd77bc64ff869d94078e81c238809a6c
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2023-12-12 22:15:13 -0500

    complete day 12 part 2

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,13 +1,12 @@
-#![allow(dead_code)]
-
-use std::fs;
-
-use itertools::Itertools;
+use std::{collections::HashMap, fs};
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("failed to read input file");
     let result = part1(&input);
     println!("part 1: {}", result);
+
+    let result = part2(&input);
+    println!("part 2: {}", result);
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -32,47 +31,94 @@ fn part1(input: &str) -> usize {
     input
         .lines()
         .map(|l| {
-            let (springs_str, criteria_str) = l.split_once(" ").expect("failed to split line");
-            let criteria: Vec<_> = criteria_str
-                .split(",")
-                .map(|s| s.parse::<usize>().expect("failed to parse criteria"))
-                .collect();
-
-            let springs: Vec<_> = springs_str.chars().map(Spring::from).collect();
-            number_arrangements(&springs, &criteria)
+            let (springs, criteria) = parse_line1(l);
+            let mut cache = HashMap::new();
+            number_arrangements(&springs, &criteria, &mut cache)
         })
         .sum()
 }
 
-fn number_arrangements(springs: &Vec<Spring>, criteria: &[usize]) -> usize {
-    if let Some(index) = springs.iter().position(|s| *s == Spring::Unknown) {
-        let mut springs_with_damaged = springs.clone();
-        springs_with_damaged[index] = Spring::Damaged;
-
-        let mut springs_with_operational = springs.clone();
-        springs_with_operational[index] = Spring::Operational;
-
-        number_arrangements(&springs_with_damaged, criteria)
-            + number_arrangements(&springs_with_operational, criteria)
-    } else {
-        if valid_arrangement(springs, criteria) {
-            1
-        } else {
-            0
-        }
-    }
+fn part2(input: &str) -> usize {
+    input
+        .lines()
+        .map(|l| {
+            let (mut springs, criteria) = parse_line2(l);
+            let mut cache = HashMap::new();
+            number_arrangements(&mut springs, &criteria, &mut cache)
+        })
+        .sum()
 }
 
-fn valid_arrangement(springs: &[Spring], criteria: &[usize]) -> bool {
-    springs
-        .iter()
-        .group_by(|&i| i)
+fn parse_line1(l: &str) -> (Vec<Spring>, Vec<usize>) {
+    let (springs_str, criteria_str) = l.split_once(" ").expect("failed to split line");
+    let criteria: Vec<_> = criteria_str
+        .split(",")
+        .map(|s| s.parse::<usize>().expect("failed to parse criteria"))
+        .collect();
+
+    let springs: Vec<_> = springs_str.chars().map(Spring::from).collect();
+    (springs, criteria)
+}
+
+fn parse_line2(l: &str) -> (Vec<Spring>, Vec<usize>) {
+    let (springs_str, criteria_str) = l.split_once(" ").expect("failed to split line");
+    let criteria_unfolded: Vec<_> = criteria_str
+        .split(",")
+        .map(|s| s.parse::<usize>().expect("failed to parse criteria"))
+        .collect();
+    let criteria_len = criteria_unfolded.len();
+
+    let criteria: Vec<_> = criteria_unfolded
         .into_iter()
-        .filter_map(|(&k, g)| match k {
-            Spring::Damaged => Some(g.count()),
-            _ => None,
-        })
-        .eq(criteria.iter().copied())
+        .cycle()
+        .take(criteria_len * 5)
+        .collect();
+
+    let springs: Vec<_> = springs_str
+        .chars()
+        .map(Spring::from)
+        .chain([Spring::Unknown])
+        .cycle()
+        .take(springs_str.len() * 5 + 4)
+        .collect();
+    (springs, criteria)
+}
+
+fn number_arrangements(
+    springs: &[Spring],
+    criteria: &[usize],
+    cache: &mut HashMap<(usize, usize), usize>,
+) -> usize {
+    if criteria.is_empty() {
+        if springs.contains(&Spring::Damaged) {
+            return 0;
+        }
+        return 1;
+    }
+
+    if springs.len() < criteria.iter().sum::<usize>() + criteria.len() - 1 {
+        return 0;
+    }
+
+    if let Some(&c) = cache.get(&(springs.len(), criteria.len())) {
+        return c;
+    }
+
+    let mut count = 0;
+    if springs[0] != Spring::Damaged {
+        count += number_arrangements(&springs[1..], criteria, cache);
+    }
+    let criteria_size = criteria[0];
+    if springs.len() == criteria_size && !springs.contains(&Spring::Operational) {
+        count += 1;
+    } else if !springs[..criteria_size].contains(&Spring::Operational)
+        && springs[criteria_size] != Spring::Damaged
+    {
+        count += number_arrangements(&springs[criteria_size + 1..], &criteria[1..], cache);
+    }
+
+    cache.insert((springs.len(), criteria.len()), count);
+    count
 }
 
 #[cfg(test)]
@@ -97,23 +143,27 @@ mod tests {
             Spring::Damaged,
         ];
         let criteria = vec![1, 1, 3];
-        let result = number_arrangements(&row, &criteria);
+        let mut cache = HashMap::new();
+        let result = number_arrangements(&row, &criteria, &mut cache);
         assert_eq!(result, 1);
     }
 
     #[test]
-    fn test_valid_arrangement() {
-        let row = vec![
-            Spring::Damaged,
-            Spring::Operational,
-            Spring::Damaged,
-            Spring::Operational,
-            Spring::Damaged,
-            Spring::Damaged,
-            Spring::Damaged,
-        ];
-        let criteria = vec![1, 1, 3];
-        let result = valid_arrangement(&row, &criteria);
-        assert_eq!(result, true);
+    fn test_unfolded_valid() {
+        let (springs, criteria) = parse_line2("???.### 1,1,3");
+        let mut cache = HashMap::new();
+        let result = number_arrangements(&springs, &criteria, &mut cache);
+        assert_eq!(result, 1);
+
+        let (springs, criteria) = parse_line2(".??..??...?##. 1,1,3");
+        let mut cache = HashMap::new();
+        let result = number_arrangements(&springs, &criteria, &mut cache);
+        assert_eq!(result, 16384);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = fs::read_to_string("test_input.txt").expect("failed to read test input file");
+        assert_eq!(part2(&input), 525152);
     }
 }
