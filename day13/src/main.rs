d091acb090602e5382b462a256b65b6b59eecc7e
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2023-12-13 20:50:31 -0500

    complete day 13 part 2

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -4,15 +4,18 @@ use std::{cmp, fs};
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("failed to load input file");
-    let result = part1(&input);
+    let result = solve(&input, 0);
     println!("part 1: {}", result);
+
+    let result = solve(&input, 1);
+    println!("part 2: {}", result);
 }
 
-fn part1(input: &str) -> usize {
+fn solve(input: &str, target: usize) -> usize {
     input
         .split("\n\n")
         .map(|p| Pattern::new(p))
-        .map(|p| p.reflection_value())
+        .map(|p| p.reflection_value(target))
         .sum()
 }
 
@@ -61,9 +64,9 @@ impl Pattern {
         }
     }
 
-    fn reflection_value(&self) -> usize {
-        let vertical = self.vertical_reflection();
-        let horizontal = self.horizontal_reflection();
+    fn reflection_value(&self, target: usize) -> usize {
+        let vertical = self.vertical_reflection(target);
+        let horizontal = self.horizontal_reflection(target);
         if let Some(v) = vertical {
             return v;
         }
@@ -73,7 +76,7 @@ impl Pattern {
         panic!("no reflection found");
     }
 
-    fn vertical_reflection(&self) -> Option<usize> {
+    fn vertical_reflection(&self, target: usize) -> Option<usize> {
         for x in 0..(self.width - 1) {
             let width = cmp::min(x + 1, self.width - x - 1);
             let result: usize = (0..width)
@@ -83,14 +86,14 @@ impl Pattern {
                     Iterator::zip(left, right).filter(|(l, r)| l != r).count()
                 })
                 .sum();
-            if result == 0 {
+            if result == target {
                 return Some(x + 1);
             }
         }
         None
     }
 
-    fn horizontal_reflection(&self) -> Option<usize> {
+    fn horizontal_reflection(&self, target: usize) -> Option<usize> {
         for y in 0..(self.height - 1) {
             let height = cmp::min(y + 1, self.height - y - 1);
             let result: usize = (0..height)
@@ -100,7 +103,7 @@ impl Pattern {
                     Iterator::zip(up, down).filter(|(u, d)| u != d).count()
                 })
                 .sum();
-            if result == 0 {
+            if result == target {
                 return Some(y + 1);
             }
         }
@@ -115,7 +118,7 @@ mod tests {
     #[test]
     fn test_part1() {
         let input = fs::read_to_string("test_input.txt").expect("failed to load input file");
-        let result = part1(&input);
+        let result = solve(&input, 0);
         assert_eq!(result, 405);
     }
 
@@ -125,35 +128,18 @@ mod tests {
         let (v, h) = input.split_once("\n\n").expect("should be 2 patterns");
 
         let vertical = Pattern::new(&v);
-        assert_eq!(vertical.horizontal_reflection(), None);
-        assert_eq!(vertical.vertical_reflection(), Some(5));
+        assert_eq!(vertical.horizontal_reflection(0), None);
+        assert_eq!(vertical.vertical_reflection(0), Some(5));
 
         let horizontal = Pattern::new(&h);
-        assert_eq!(horizontal.horizontal_reflection(), Some(4));
-        assert_eq!(horizontal.vertical_reflection(), None);
+        assert_eq!(horizontal.horizontal_reflection(0), Some(4));
+        assert_eq!(horizontal.vertical_reflection(0), None);
     }
 
-    //     #[test]
-    //     fn test_bad_reflection() {
-    //         let input = r#"...##..##....
-    // ..##.###...#.
-    // ...#.#.#.###.
-    // ..#...#.#####
-    // ...####...###
-    // ##.######.##.
-    // .....##.#.##.
-    // ...##.#...#.#
-    // ####..#.#####
-    // ....#####.##.
-    // ...####..#..#
-    // .....#.###..#
-    // ...#..#.#...#
-    // ..#.#.#...##.
-    // ....#.#...##.
-    // "#;
-    //         let pattern = Pattern::new(input);
-    //
-    //         assert_eq!(pattern.horizontal_reflection(), Some(4));
-    //         assert_eq!(pattern.vertical_reflection(), None);
-    //     }
+    #[test]
+    fn test_part2() {
+        let input = fs::read_to_string("test_input.txt").expect("failed to load input file");
+        let result = solve(&input, 1);
+        assert_eq!(result, 400);
+    }
 }
