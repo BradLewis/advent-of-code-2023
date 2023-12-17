5bfcc7d87d4b5bb7a20b00bf8840b322f4b06221
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2023-12-17 11:10:31 -0500

    complete day 17 part 2

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -2,15 +2,18 @@
 
 use std::{
     cmp::Reverse,
-    collections::{BinaryHeap, HashSet},
+    collections::{BinaryHeap, HashMap, HashSet},
     fs,
     ops::Index,
 };
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("failed to read input file");
-    let result = part1(&input);
+    let result = solve(&input, 1, 3);
     println!("part 1: {result}");
+
+    let result = solve(&input, 4, 10);
+    println!("part 2: {result}");
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -76,195 +79,184 @@ impl State {
     }
 }
 
-fn part1(input: &str) -> usize {
+fn solve(input: &str, min_same_direction_count: i32, max_same_direction_count: i32) -> usize {
     let map = Map::new(input);
     let start_position = Position { x: 0, y: 0 };
     let target_position = Position {
         x: map.width - 1,
         y: map.height - 1,
     };
-    dijkstra(&map, start_position, target_position)
+    dijkstra(
+        &map,
+        start_position,
+        target_position,
+        min_same_direction_count,
+        max_same_direction_count,
+    )
 }
 
-fn dijkstra(map: &Map, start_position: Position, target_position: Position) -> usize {
-    let mut distances = vec![vec![usize::MAX; map.width]; map.height];
-    distances[start_position.y][start_position.x] = 0;
+fn dijkstra(
+    map: &Map,
+    start_position: Position,
+    target_position: Position,
+    min_same_direction_count: i32,
+    max_same_direction_count: i32,
+) -> usize {
+    let mut distances = HashMap::new();
     let mut queue = BinaryHeap::new();
-    let start_state = State::new(start_position.x, start_position.y, Direction::Right, 0);
-    let mut best = usize::MAX;
+    let start_state_right = State::new(start_position.x, start_position.y, Direction::Right, 0);
+    let start_state_down = State::new(start_position.x, start_position.y, Direction::Down, 0);
     let mut seen = HashSet::new();
-    queue.push(Reverse((0, start_state)));
-    while let Some(Reverse((_, state))) = queue.pop() {
+    distances.insert(start_state_right, 0);
+    distances.insert(start_state_down, 0);
+    queue.push(Reverse((0, start_state_right)));
+    queue.push(Reverse((0, start_state_down)));
+    while let Some(Reverse((current_distance, state))) = queue.pop() {
         seen.insert(state);
-        let current_distance = distances[state.position.y][state.position.x];
-        let neighbours = get_neighbours(map, &state, 0, 3);
+        let neighbours = get_neighbours(
+            map,
+            state,
+            min_same_direction_count,
+            max_same_direction_count,
+        );
         for neighbour in neighbours {
             if seen.contains(&neighbour) {
                 continue;
             }
-            let neighbour_distance = distances[neighbour.position.y][neighbour.position.x];
             let distance = current_distance + map[neighbour.position];
-            if distance < neighbour_distance {
+            let neighbour_distance = distances.get(&neighbour).unwrap_or(&usize::MAX);
+            if distance < *neighbour_distance {
                 if neighbour.position == target_position {
-                    println!("found target, {distance}");
-                    best = best.min(distance);
+                    return distance;
                 }
-                distances[neighbour.position.y][neighbour.position.x] = distance;
+                distances.insert(neighbour, distance);
                 queue.push(Reverse((distance, neighbour)));
             }
         }
     }
-    best
+    usize::MAX
 }
 
 fn get_neighbours(
     map: &Map,
-    state: &State,
+    state: State,
     min_same_direction_count: i32,
     max_same_direction_count: i32,
 ) -> Vec<State> {
     let mut neighbours = Vec::new();
     match state.direction {
         Direction::Up => {
-            if state.same_direction_count < min_same_direction_count {
+            if state.position.y > 0 && state.same_direction_count < max_same_direction_count {
                 neighbours.push(State::new(
                     state.position.x,
                     state.position.y - 1,
                     Direction::Up,
                     state.same_direction_count + 1,
                 ));
-            } else {
-                if state.position.y > 0 && state.same_direction_count < max_same_direction_count {
-                    neighbours.push(State::new(
-                        state.position.x,
-                        state.position.y - 1,
-                        Direction::Up,
-                        state.same_direction_count + 1,
-                    ));
-                }
-                if state.position.x > min_same_direction_count as usize {
+            }
+            if state.same_direction_count >= min_same_direction_count {
+                if state.position.x > (min_same_direction_count - 1) as usize {
                     neighbours.push(State::new(
                         state.position.x - 1,
                         state.position.y,
                         Direction::Left,
-                        0,
+                        1,
                     ));
                 }
-                if state.position.x < map.width - 1 - min_same_direction_count as usize {
+                if state.position.x < map.width - min_same_direction_count as usize {
                     neighbours.push(State::new(
                         state.position.x + 1,
                         state.position.y,
                         Direction::Right,
-                        0,
+                        1,
                     ));
                 }
             }
         }
         Direction::Down => {
-            if state.same_direction_count < min_same_direction_count {
+            if state.position.y < map.height - 1
+                && state.same_direction_count < max_same_direction_count
+            {
                 neighbours.push(State::new(
                     state.position.x,
                     state.position.y + 1,
                     Direction::Down,
                     state.same_direction_count + 1,
                 ));
-            } else {
-                if state.position.y < map.height - 1
-                    && state.same_direction_count < max_same_direction_count
-                {
-                    neighbours.push(State::new(
-                        state.position.x,
-                        state.position.y + 1,
-                        Direction::Down,
-                        state.same_direction_count + 1,
-                    ));
-                }
-                if state.position.x > min_same_direction_count as usize {
+            }
+            if state.same_direction_count >= min_same_direction_count {
+                if state.position.x > (min_same_direction_count - 1) as usize {
                     neighbours.push(State::new(
                         state.position.x - 1,
                         state.position.y,
                         Direction::Left,
-                        0,
+                        1,
                     ));
                 }
-                if state.position.x < map.width - 1 - min_same_direction_count as usize {
+                if state.position.x < map.width - min_same_direction_count as usize {
                     neighbours.push(State::new(
                         state.position.x + 1,
                         state.position.y,
                         Direction::Right,
-                        0,
+                        1,
                     ));
                 }
             }
         }
         Direction::Left => {
-            if state.same_direction_count < min_same_direction_count {
+            if state.position.x > 0 && state.same_direction_count < max_same_direction_count {
                 neighbours.push(State::new(
                     state.position.x - 1,
                     state.position.y,
                     Direction::Left,
                     state.same_direction_count + 1,
                 ));
-            } else {
-                if state.position.x > 0 && state.same_direction_count < max_same_direction_count {
-                    neighbours.push(State::new(
-                        state.position.x - 1,
-                        state.position.y,
-                        Direction::Left,
-                        state.same_direction_count + 1,
-                    ));
-                }
-                if state.position.y > min_same_direction_count as usize {
+            }
+            if state.same_direction_count >= min_same_direction_count {
+                if state.position.y > (min_same_direction_count - 1) as usize {
                     neighbours.push(State::new(
                         state.position.x,
                         state.position.y - 1,
                         Direction::Up,
-                        0,
+                        1,
                     ));
                 }
-                if state.position.y < map.height - 1 - min_same_direction_count as usize {
+                if state.position.y < map.height - min_same_direction_count as usize {
                     neighbours.push(State::new(
                         state.position.x,
                         state.position.y + 1,
                         Direction::Down,
-                        0,
+                        1,
                     ));
                 }
             }
         }
         Direction::Right => {
-            if state.same_direction_count < min_same_direction_count {
+            if state.position.x < map.width - 1
+                && state.same_direction_count < max_same_direction_count
+            {
                 neighbours.push(State::new(
                     state.position.x + 1,
                     state.position.y,
                     Direction::Right,
                     state.same_direction_count + 1,
                 ));
-            } else {
-                if state.position.x < map.width - 1
-                    && state.same_direction_count < max_same_direction_count
-                {
-                    neighbours.push(State::new(
-                        state.position.x + 1,
-                        state.position.y,
-                        Direction::Right,
-                        state.same_direction_count + 1,
-                    ));
-                }
-                if state.position.y > min_same_direction_count as usize {
+            }
+            if state.same_direction_count >= min_same_direction_count {
+                if state.position.y > (min_same_direction_count - 1) as usize {
                     neighbours.push(State::new(
                         state.position.x,
                         state.position.y - 1,
                         Direction::Up,
-                        0,
+                        1,
                     ));
                 }
-                if state.position.y < map.height - 1 - min_same_direction_count as usize {
+                if state.position.y < map.height - min_same_direction_count as usize {
                     neighbours.push(State::new(
                         state.position.x,
                         state.position.y + 1,
                         Direction::Down,
-                        0,
+                        1,
                     ));
                 }
             }
@@ -281,33 +273,18 @@ mod tests {
     #[test]
     fn test_part1() {
         let input = fs::read_to_string("test_input.txt").expect("failed to read test input file");
-        assert_eq!(part1(&input), 102);
+        assert_eq!(solve(&input, 1, 3), 102);
     }
 
     #[test]
-    fn test_get_neighbours() {
+    fn test_part2() {
         let input = fs::read_to_string("test_input.txt").expect("failed to read test input file");
-        let map = Map::new(&input);
-        let state = State::new(0, 0, Direction::Right, 0);
-        let neighbours = get_neighbours(&map, &state, 0, 3);
-        assert_eq!(neighbours.len(), 2);
-        assert_eq!(
-            neighbours[0],
-            State::new(1, 0, Direction::Right, 1),
-            "neighbour 0"
-        );
-        assert_eq!(
-            neighbours[1],
-            State::new(0, 1, Direction::Down, 0),
-            "neighbour 1"
-        );
+        assert_eq!(solve(&input, 4, 10), 94);
+    }
 
-        let state = State::new(5, 3, Direction::Right, 1);
-        let neighbours = get_neighbours(&map, &state, 0, 3);
-        assert_eq!(neighbours.len(), 3);
-
-        let state = State::new(5, 3, Direction::Right, 3);
-        let neighbours = get_neighbours(&map, &state, 0, 3);
-        assert_eq!(neighbours.len(), 2);
+    #[test]
+    fn test_part2_2() {
+        let input = fs::read_to_string("test_input2.txt").expect("failed to read test input file");
+        assert_eq!(solve(&input, 4, 10), 71);
     }
 }
