d16af9744ce902d1baf887c8cfcd81f56814a6e4
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2023-12-18 22:32:35 -0500

    complete day 18 part 2

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,126 +1,6 @@
 #![allow(dead_code)]
 
-use std::{
-    fs,
-    ops::{Index, IndexMut},
-};
-
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-enum Tile {
-    Empty,
-    Filled,
-}
-
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-struct Position {
-    x: usize,
-    y: usize,
-}
-
-impl Position {
-    fn up(&self) -> Self {
-        Self {
-            x: self.x,
-            y: self.y + 1,
-        }
-    }
-    fn down(&self) -> Self {
-        Self {
-            x: self.x,
-            y: self.y - 1,
-        }
-    }
-    fn left(&self) -> Self {
-        Self {
-            x: self.x - 1,
-            y: self.y,
-        }
-    }
-    fn right(&self) -> Self {
-        Self {
-            x: self.x + 1,
-            y: self.y,
-        }
-    }
-}
-
-#[derive(Debug)]
-struct Map {
-    map: Vec<Vec<Tile>>,
-    width: usize,
-    height: usize,
-}
-
-impl Map {
-    fn new(edges: &[Edge], start_pos: Position, width: usize, height: usize) -> Self {
-        let mut map = vec![vec![Tile::Empty; width]; height];
-        let mut current_pos = start_pos;
-        for edge in edges {
-            let mut x = current_pos.x;
-            let mut y = current_pos.y;
-            for _ in 0..edge.length {
-                match edge.direction {
-                    Direction::Up => y -= 1,
-                    Direction::Down => y += 1,
-                    Direction::Left => x -= 1,
-                    Direction::Right => x += 1,
-                }
-                map[y][x] = Tile::Filled;
-            }
-            current_pos = Position { x, y };
-        }
-        Self { map, width, height }
-    }
-
-    fn flood_fill(&mut self, position: Position) {
-        self[position] = Tile::Filled;
-        if position.x > 0 && self[position.up()] == Tile::Empty {
-            self.flood_fill(position.up());
-        }
-        if position.x < self.width - 1 && self[position.down()] == Tile::Empty {
-            self.flood_fill(position.down());
-        }
-        if position.y > 0 && self[position.left()] == Tile::Empty {
-            self.flood_fill(position.left());
-        }
-        if position.y < self.height - 1 && self[position.right()] == Tile::Empty {
-            self.flood_fill(position.right());
-        }
-    }
-
-    fn count_filled(&self) -> usize {
-        self.map
-            .iter()
-            .map(|row| row.iter().filter(|tile| **tile == Tile::Filled).count())
-            .sum()
-    }
-
-    fn print(&self) {
-        for row in &self.map {
-            for tile in row {
-                match tile {
-                    Tile::Empty => print!("."),
-                    Tile::Filled => print!("#"),
-                }
-            }
-            println!();
-        }
-    }
-}
-
-impl Index<Position> for Map {
-    type Output = Tile;
-
-    fn index(&self, position: Position) -> &Self::Output {
-        &self.map[position.y][position.x]
-    }
-}
-
-impl IndexMut<Position> for Map {
-    fn index_mut(&mut self, position: Position) -> &mut Self::Output {
-        &mut self.map[position.y][position.x]
-    }
-}
+use std::fs;
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 enum Direction {
@@ -137,104 +17,126 @@ impl From<&str> for Direction {
             "D" => Direction::Down,
             "L" => Direction::Left,
             "R" => Direction::Right,
+            "0" => Direction::Right,
+            "1" => Direction::Down,
+            "2" => Direction::Left,
+            "3" => Direction::Up,
             _ => panic!("invalid direction"),
         }
     }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+struct Position {
+    x: i64,
+    y: i64,
 }
 
 #[derive(Debug)]
 struct Edge {
     direction: Direction,
     length: usize,
-    colour: u32,
 }
 
 impl Edge {
-    fn new(direction: Direction, length: usize, colour: u32) -> Self {
-        Self {
-            direction,
-            length,
-            colour,
-        }
+    fn new(direction: Direction, length: usize) -> Self {
+        Self { direction, length }
     }
 }
 
 impl From<&str> for Edge {
     fn from(s: &str) -> Self {
-        let mut s = s.splitn(3, " ");
-        let direction = Direction::from(s.next().expect("expected direction"));
-        let length = s
-            .next()
-            .expect("expected length")
+        let (direction_str, s) = s.split_once(" ").expect("expected direction and length");
+        let direction = Direction::from(direction_str);
+        let (length_str, _) = s.split_once(" ").expect("expected length");
+        let length = length_str
             .parse::<usize>()
             .expect("expected length to be a number");
-        let colour = &s.next().expect("expected colour")[2..8];
-        let colour = u32::from_str_radix(colour, 16).expect("expected colour to be a hex number");
-        Self::new(direction, length, colour)
+        Self::new(direction, length)
     }
 }
 
-fn get_bounds(edges: &[Edge]) -> (i32, i32, i32, i32) {
-    let mut min_x = 0;
-    let mut min_y = 0;
-    let mut max_x = 0;
-    let mut max_y = 0;
-    let mut x = 0;
-    let mut y = 0;
-    for edge in edges {
-        match edge.direction {
-            Direction::Up => y -= edge.length as i32,
-            Direction::Down => y += edge.length as i32,
-            Direction::Left => x -= edge.length as i32,
-            Direction::Right => x += edge.length as i32,
-        }
-        if x < min_x {
-            min_x = x;
-        }
-        if x > max_x {
-            max_x = x;
-        }
-        if y < min_y {
-            min_y = y;
-        }
-        if y > max_y {
-            max_y = y;
-        }
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+struct Vertex {
+    position: Position,
+}
+
+impl Vertex {
+    fn new(position: Position) -> Self {
+        Self { position }
     }
-    (min_x, min_y, max_x, max_y)
 }
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("failed to read input file");
     let result = part1(&input);
     println!("part 1: {}", result);
+
+    let result = part2(&input);
+    println!("part 2: {}", result);
 }
 
-fn part1(input: &str) -> usize {
+fn area(vertices: &[Vertex]) -> i64 {
+    let mut area = 0;
+    for i in 0..(vertices.len() - 1) {
+        let v1 = vertices[i];
+        let v2 = vertices[i + 1];
+        area += (v1.position.y + v2.position.y) * (v1.position.x - v2.position.x);
+    }
+    area / 2
+}
+
+fn boundary_length(vertices: &[Vertex]) -> i64 {
+    let v1 = vertices[0];
+    let v2 = vertices[vertices.len() - 1];
+    let mut length = (v1.position.x - v2.position.x).abs() + (v1.position.y - v2.position.y).abs();
+    for i in 0..(vertices.len() - 1) {
+        let v1 = vertices[i];
+        let v2 = vertices[i + 1];
+        length += (v1.position.x - v2.position.x).abs();
+        length += (v1.position.y - v2.position.y).abs();
+    }
+    length
+}
+
+fn solve(edges: &[Edge]) -> i64 {
+    let mut vertices = Vec::with_capacity(edges.len() + 1);
+
+    let mut x: i64 = 0;
+    let mut y: i64 = 0;
+    for edge in edges {
+        match edge.direction {
+            Direction::Up => y -= edge.length as i64,
+            Direction::Down => y += edge.length as i64,
+            Direction::Left => x -= edge.length as i64,
+            Direction::Right => x += edge.length as i64,
+        }
+        let vertex = Vertex::new(Position { x, y });
+        vertices.push(vertex);
+    }
+
+    area(&vertices) + boundary_length(&vertices) / 2 + 1
+}
+
+fn part1(input: &str) -> i64 {
     let edges: Vec<_> = input.lines().map(Edge::from).collect();
-    let (min_x, min_y, max_x, max_y) = get_bounds(&edges);
-    println!("min_x: {}", min_x);
-    println!("min_y: {}", min_y);
-    println!("max_x: {}", max_x);
-    println!("max_y: {}", max_y);
-    let width = (max_x - min_x) as usize + 1;
-    let height = (max_y - min_y) as usize + 1;
-    let start_pos = Position {
-        x: -min_x as usize,
-        y: -min_y as usize,
-    };
+    solve(&edges)
+}
 
-    let mut map = Map::new(&edges, start_pos, width, height);
-    map.print();
+fn part2(input: &str) -> i64 {
+    let edges: Vec<_> = input
+        .lines()
+        .map(|l| {
+            let s = l.split(" ").last().expect("expected colour");
+            let length =
+                usize::from_str_radix(&s[2..7], 16).expect("expected colour to be a hex number");
+            let last_digit = &s[7..8];
+            let direction = Direction::from(last_digit);
+            Edge::new(direction, length)
+        })
+        .collect();
 
-    let fill_start_pos = Position {
-        x: start_pos.x + 1,
-        y: start_pos.y + 1,
-    };
-    map.flood_fill(fill_start_pos);
-    map.print();
-
-    map.count_filled()
+    solve(&edges)
 }
 
 #[cfg(test)]
@@ -245,5 +147,11 @@ mod tests {
     fn test_part1() {
         let input = fs::read_to_string("test_input.txt").expect("failed to read test input file");
         assert_eq!(part1(&input), 62);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = fs::read_to_string("test_input.txt").expect("failed to read test input file");
+        assert_eq!(part2(&input), 952408144115);
     }
 }
